3bfcf55dd339d1e69bee8317c0f72ba4488f2c89
alex90271 <alex.alder1@gmail.com>
2022-04-15 20:02:23 -0600

    initial

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:alderautomationsdotcom/views/home/home_view.dart';
 import 'package:flutter/material.dart';
+import 'views/home/home_view.dart';
 
 void main() => runApp(const MyApp());
 
